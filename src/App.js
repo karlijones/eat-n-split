2e279dd560f737518902648948e83a1e1ff3716d
karlijones <karlijones1984@gmail.com>
2024-11-02 17:21:11 -0500

    placeholder image

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function Friend({friend}) {
 
 function FormAddFriend() {
   const [name, setName] = useState();
-  const [image, setImage] = useState();
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
 
   return (
     <form className="form-add-friend">
