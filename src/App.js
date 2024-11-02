f50fb8d069c9acae3f15f03879b3544b62162fb1
karlijones <karlijones1984@gmail.com>
2024-11-02 12:22:57 -0500

    labels for inputs for bill split form

--- src/App.js
+++ src/App.js
@@ -96,6 +96,17 @@ function FormSplitBill() {
   return (
   <form className="form-split-bill">
     <h2>Split a bill with X</h2>
+
+    <label>💰 Bill value</label>
+      <input type="text" />
+    
+    <label>🧍‍♀️ Your expense</label>
+      <input type="text" />
+
+    <label>👫 X's expense</label>
+      <input type="text" />
+
+      <Button>Split bill</Button>
   </form>
   );
 }
