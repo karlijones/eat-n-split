b5bea0f31a57ae4f95476a5821e9c24d9e7b9bcd
karlijones <karlijones1984@gmail.com>
2024-11-03 15:12:25 -0600

    adding useState

--- src/App.js
+++ src/App.js
@@ -158,6 +158,11 @@ function FormAddFriend({onAddFriend}) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+  
+  
   return (
   <form className="form-split-bill">
     <h2>Split a bill with {selectedFriend.name}</h2>
