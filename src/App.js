00dbd479419354047fc8a45d7ad998805e1291bf
karlijones <karlijones1984@gmail.com>
2024-11-03 15:23:48 -0600

    ternary operator to calculate friend expense

--- src/App.js
+++ src/App.js
@@ -160,6 +160,7 @@ function FormAddFriend({onAddFriend}) {
 function FormSplitBill({ selectedFriend }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
   const [whoIsPaying, setWhoIsPaying] = useState("user");
   
   
